a87c4860f887eeb13f4ec068f3b2735342714007
Marcia Marques <marcia.marques@alumni.usp.br>
2021-12-02 23:15:58 -0300

    Add Flag and Event models

--- project/logbook/admin.py
+++ project/logbook/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Flag, Event, EventFile, EventImage
+
+
+class EventFileInline(admin.TabularInline):
+    model = EventFile
+
+
+class EventImageInline(admin.TabularInline):
+    model = EventImage
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        EventFileInline,
+        EventImageInline,
+    ]
+    readonly_fields = ('author', 'created_on', 'updated_on')
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user.first_name + ' ' + request.user.last_name
+        obj.save()
+
+
+admin.site.register(Flag)
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventFile)
--- project/logbook/models.py
+++ project/logbook/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Flag(models.Model):
+    flag = models.CharField(max_length=3)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.flag
+
+
+class Event(models.Model):
+    author = models.CharField(max_length=100, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    event_date = models.DateField(help_text="Please use the following format: YYYY-MM-DD.")
+    description = models.TextField(max_length=1000)
+    invalid_data = models.BooleanField(default=False)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    flags = models.ManyToManyField(Flag, blank=True)
+
+    def __str__(self):
+        return self.description
+
+
+class EventFile(models.Model):
+    file = models.FileField(upload_to="logbook/files/")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.file.storage, self.file.path
+        super(EventFile, self).delete(*args, **kwargs)
+        storage.delete(path)
+
+
+class EventImage(models.Model):
+    image = models.ImageField(upload_to="logbook/images/")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.image.storage, self.image.path
+        super(EventImage, self).delete(*args, **kwargs)
+        storage.delete(path)
