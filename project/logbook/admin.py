f73a365d77ccecb247a2e4f6865a46aa5880daf5
Marcia Marques <marcia.marques@alumni.usp.br>
2021-12-07 23:13:48 -0300

    Add new models

--- project/logbook/admin.py
+++ project/logbook/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 
-from .models import Flag, Event, EventFile
+from .models import Instrument, InstrumentFile,\
+                    Station, StationFile,\
+                    Campaign, CampaignFile,\
+                    Flag, \
+                    Event, EventFile,\
+                    Logbook
+
+
+class InstrumentFileInline(admin.TabularInline):
+    model = InstrumentFile
+
+
+class InstrumentAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+    inlines = [InstrumentFileInline]
+
+
+class StationFileInline(admin.TabularInline):
+    model = StationFile
+
+
+class StationAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+    inlines = [StationFileInline]
+
+
+class CampaignFileInline(admin.TabularInline):
+    model = CampaignFile
+
+
+class CampaignAdmin(admin.ModelAdmin):
+    readonly_fields = ('name', 'slug',)
+    inlines = [CampaignFileInline]
 
 
 class EventFileInline(admin.TabularInline):
@@ -8,12 +40,23 @@ class EventFileInline(admin.TabularInline):
 
 
 class EventAdmin(admin.ModelAdmin):
-    inlines = [
-        EventFileInline,
-    ]
-    list_display = ('event_date', 'description')
+    readonly_fields = ('name', 'slug',)
+    inlines = [EventFileInline]
 
 
+class EventInline(admin.TabularInline):
+    model = Event
+
+
+class LogbookAdmin(admin.ModelAdmin):
+    readonly_fields = ('name', 'slug',)
+    inlines = [EventInline]
+
+
+admin.site.register(Instrument, InstrumentAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Flag)
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventFile)
+admin.site.register(Logbook, LogbookAdmin)
--- project/logbook/models.py
+++ project/logbook/models.py
@@ -1,4 +1,96 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=50)
+    serial_number = models.CharField(max_length=50)
+    mobile_campaign = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class InstrumentFile(models.Model):
+    file = models.FileField(upload_to="files/instrument/")
+    description = models.CharField(max_length=500)
+    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.file.storage, self.file.path
+        super(InstrumentFile, self).delete(*args, **kwargs)
+        storage.delete(path)
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    latitude = models.DecimalField(max_digits=11, decimal_places=6)
+    longitude = models.DecimalField(max_digits=11, decimal_places=6)
+    elevation = models.DecimalField(max_digits=4, decimal_places=0)
+    instruments = models.ManyToManyField(Instrument, blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class StationFile(models.Model):
+    file = models.FileField(upload_to="files/station/")
+    description = models.CharField(max_length=500)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.file.storage, self.file.path
+        super(StationFile, self).delete(*args, **kwargs)
+        storage.delete(path)
+
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
+    date = models.DateField(help_text="Please use the following format: YYYY-MM-DD.", null=True)
+    mobile_campaign = models.BooleanField(default=False)
+    description = models.TextField(max_length=1000)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = self.station.name + " " + self.instrument.name + " " + str(self.date).replace('-', '')[:-2]
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class CampaignFile(models.Model):
+    file = models.FileField(upload_to="files/campaign/")
+    description = models.CharField(max_length=500)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.file.storage, self.file.path
+        super(CampaignFile, self).delete(*args, **kwargs)
+        storage.delete(path)
 
 
 class Flag(models.Model):
@@ -9,20 +101,54 @@ class Flag(models.Model):
         return self.flag
 
 
+class Logbook(models.Model):
+    name = models.CharField(max_length=50)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, blank=True)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = "Logbook " + self.campaign.name
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
 class Event(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    logbook = models.ForeignKey(Logbook, on_delete=models.CASCADE)
     event_date = models.DateField(help_text="Please use the following format: YYYY-MM-DD.")
     description = models.TextField(max_length=1000)
     invalid_data = models.BooleanField(default=False)
     start_date = models.DateTimeField(null=True, blank=True)
     end_date = models.DateTimeField(null=True, blank=True)
     flags = models.ManyToManyField(Flag, blank=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
-        return self.description
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if Event.objects.filter(event_date=self.event_date).count() > 0:
+            n = Event.objects.filter(event_date=self.event_date).count()
+        else:
+            n = 0
+        self.name = self.logbook.name + " " + str(self.event_date) + " " + f"{n:02d}"
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+def event_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "files/event/%s.%s" % (instance.event.name, ext)
+    return filename
 
 
 class EventFile(models.Model):
-    file = models.FileField(upload_to="logbook/files/")
+    file = models.FileField(upload_to=event_file_path)
+    description = models.CharField(max_length=500)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
